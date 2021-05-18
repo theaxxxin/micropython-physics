ceaea7d41e58d14486c37b45b80a826e9e9908b9
theaxxxin <84284439+theaxxxin@users.noreply.github.com>
2021-05-19 04:49:34 +0530

    Update particles.py

--- particles.py
+++ particles.py
@@ -2,10 +2,10 @@
 ##--ESP32 with 1.3Inch OLED Via i2c--##
 #
 #
-#--- First of all, this project could not have been completed without help from various members of the global programming community. As and when people help me, I will add them to this list which I hope will be maintained
-#--- Special thanks to Mattia Conti, Deshipu (Radomir Dopieralski), and 20GOTO10 from discord. If you all ever see this, you guys rock!
+# --- First of all, this project could not have been completed without help from various members of the global programming community. As and when people help me, I will add them to this list which I hope will be maintained
+# --- Special thanks to Mattia Conti, Deshipu (Radomir Dopieralski), and 20GOTO10 from discord. If you all ever see this, you guys rock!
 #
-#----- CODE FINALLY WORKING!----
+# ----- CODE FINALLY WORKING!----
 
 from machine import Pin, I2C
 
@@ -27,7 +27,7 @@ display.fill(0)
 
 class particle:
 
-    def __init__(self, iposition, mass, velocity,index):
+    def __init__(self, iposition, mass, velocity, index):
         self.position = iposition
         self.mass = mass
         self.velocity = velocity
@@ -43,20 +43,14 @@ class particle:
         self.update()
 
     def update(self):
-        text = "X pos :{0},yPos: {1}".format(self.position[0], self.position[1])
-        print(text)
+        #text = "X pos :{0},yPos: {1}".format(self.position[0], self.position[1])
+        #print(text)
         ## The code below draws a pixel at the x and y position of the particle when called (Last parameter is colour, can either be 0 or 1)
-        display.pixel(int(round(self.position[0])), int(round(self.position[1])),1)
-    
-        
+        display.pixel(int(round(self.position[0])), int(round(self.position[1])), 1)
+
     def clear(self):
         ## The code below draws a pixel at the x and y position of the particle when called (Last parameter is colour, can either be 0 or 1)
-        display.pixel(int(round(self.position[0])), int(round(self.position[1])),0)
-         
-        
-
-
-
+        display.pixel(int(round(self.position[0])), int(round(self.position[1])), 0)
 
     def intersection(self, particles, nparticles):
         for i in range(nparticles):
@@ -64,9 +58,9 @@ class particle:
                 x = self.position[0] - particles[i].position[0]
                 y = self.position[1] - particles[i].position[1]
                 if x ** 2 + y ** 2 < 1:
-                    print('true')
+                    #print('true')
                     return [True, i]
-        print('false')
+        #print('false')
         return [False]
 
     def scatter(self, particles, nparticles):
@@ -94,30 +88,110 @@ class particle:
                 1]) / totalmass
 
 
-Nparticles = 2
+class fixsurface:
+
+    def __init__(self, positionstart, positionend):
+        self.position = positionstart + positionend
+
+        # where the surface starts and ends
+        self.positionstart = positionstart
+        self.positionend = positionend
+        # self.length = math.sqrt((positionend[1]-positionstart[1])**2 + (positionend[0]-positionstart[0]))
+        self.xtype = True
+        self.m = 0
+        self.q = 0
+        self.alpha = 0
+        self.calculatemq = 0
+        self.update()
+
+    def calculatemq(self):
+        # Calculate coefficient that descrives the segment as a straig line (Angular Coeffecient)
+        deltay = self.positionend[1] - self.positionstart[1]
+        deltax = self.positionend[0] - self.positionstart[0]
+        if abs(deltax) > 0:
+            self.m = deltay / deltax
+            self.q - self.positionstart[1] - self.m * self.positionstart[0]
+            self.alpha = math.atan(self.m)
+        else:
+            self.xtype = False
+            self.alpha = math.pi / 2
+
+    def update(self):
+        display.line(int(round(self.positionstart[0])), int(round(self.positionstart[1])),
+                     int(round(self.positionend[0])), int(round(self.positionend[1])), 1)
+
+    def intersection(self, particles, nparticles):
+        # Get the position where particles hit the segment
+        for i in range(nparticles):
+            if self.xtype:
+                y = self.m * particles[i].position[0] + self.q
+                if abs(y - particles[i].position[1]) < 40:
+                    return [True, i]
+                else:
+                    x = self.positionstart[0]
+                    if abs(x - particles[i].position[0]) < 40:
+                        return [True, i]
+
+        return [False]
+
+    def scatter(self, particles, nparticles):
+        where = self.intersection(particles, nparticles)
+        if where[0]:
+            tempvx = particles[where[1]].velocity[0]
+            tempvy = particles[where[1]].velocity[1]
+            particles[where[1]].velocity[0] = math.cos(2 * self.alpha) * tempvx + math.sin(2 * self.alpha) * tempvy
+            particles[where[1]].velocity[1] = math.cos(2 * self.alpha) * tempvx + math.cos(2 * self.alpha) * tempvy
+
+
+Nparticles = 20
 rangeparticles = range(Nparticles)
 mainparticles = []
-mainparticles += [particle([10,30],30,[1,0],0)]
-mainparticles += [particle([80,30],400,[-1,0],1)]
+for i in rangeparticles:
+    mainparticles += [particle([random.uniform(3, 126), random.uniform(3, 61)], 5,
+                               [random.uniform(-2, 2), random.uniform(-2, 2)], i)]
+
+# mainparticles += [particle([10, 30], 30, [1, 0], 0)]
+# mainparticles += [particle([80, 30], 400, [-1, 0], 1)]
+
+
+# Create Surfaces
+Nsurfaces = 4
+rangesurfaces = range(Nsurfaces)
+surfaces = []
+surfaces += [fixsurface([2, 2], [126, 2])]
+surfaces += [fixsurface([126,2], [126, 62])]
+surfaces += [fixsurface([126,62], [2, 62])]
+surfaces += [fixsurface([2, 62], [2, 2])]
 
 while True:
-    
-    #Clear, Scatter, move and update
-    
+
+    # Clear, Scatter, move and update
+
     for i in rangeparticles:
+        # CLEAR PARTICLES
         mainparticles[i].clear()
-        
-        
+
+    for i in rangesurfaces:
+        # DRAW SURFACES
+        surfaces[i].update()
+        surfaces[i].scatter(mainparticles, Nparticles)
+    
     for i in rangeparticles:
-        mainparticles[i].scatter(mainparticles,Nparticles)
-       
+      
+        mainparticles[i].scatter(mainparticles, Nparticles)
 
     for i in rangeparticles:
         mainparticles[i].isscatter = True
         mainparticles[i].move()
 
     for i in rangeparticles:
-         mainparticles[i].update()
+        mainparticles[i].update()
     display.show()
+
+
+
+
+
+
 
 
